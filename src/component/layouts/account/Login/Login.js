2dd4a28515ffcb5e1478faccf0d00a3c20600a8b
VanHoang610 <137982306+VanHoang610@users.noreply.github.com>
2024-05-17 15:36:54 +0700

    final

--- src/component/layouts/account/Login/Login.js
+++ src/component/layouts/account/Login/Login.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Link, useNavigate  } from "react-router-dom";
-import { useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
+import Button from '../../../buttons/index'
 import classNames from "classnames/bind";
 import styles from "./Login.module.scss";
-import logo from "../../../../assets/images/logoYumhub.png"
-import icon_username from "../../../../assets/images/iconUsername.png"
-import icon_password from "../../../../assets/images/iconPassword.png"
-import cricle from "../../../../assets/images/icon_circle.png"
-import eye from "../../../../assets/images/icon_eye.png"
+import logo from "../../../../assets/images/logoYumhub.png";
+import icon_username from "../../../../assets/images/iconUsername.png";
+import icon_password from "../../../../assets/images/iconPassword.png";
+import cricle from "../../../../assets/images/icon_circle.png";
+import eye from "../../../../assets/images/icon_eye.png";
 
 const cx = classNames.bind(styles);
 function Login() {
@@ -27,8 +28,8 @@ function Login() {
       password: "123",
     },
   ]);
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   console.log(username);
   const handleSubmit = () => {
@@ -38,9 +39,9 @@ function Login() {
     if (user) {
       navigate("/otp");
     } else {
-      alert('Invalid username or password');
+      alert("Invalid username or password");
     }
-  }
+  };
   return (
     <div className={cx("container")}>
       <div className={cx("leftBackground")}>
@@ -65,20 +66,30 @@ function Login() {
         </div>
         <div className={cx("enterInfo")}>
           <img src={icon_username} alt="Username" className={cx("iconStart")} />
-          <input placeholder="lv.hoang610@gmail.com" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <input
+            placeholder="lv.hoang610@gmail.com"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <img src={cricle} alt="Cricle" className={cx("iconEnd")} />
         </div>
         <div className={cx("enterInfo")}>
           <img src={icon_password} alt="Password" className={cx("iconStart")} />
-          <input placeholder="lv.hoang0610" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input
+            placeholder="lv.hoang0610"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <img src={eye} alt="Cricle" className={cx("iconEnd")} />
         </div>
-        <div className={cx("btnLogin")} onClick={handleSubmit}>
-          <p className={cx("textLogin")}>LOGIN</p>
+        <div className={cx("btnLogin")}>
+          <Button login onClick={handleSubmit}>
+            LOGIN
+          </Button>
         </div>
         <div className={cx("rememberPassword")}>
           <p className={cx("textRemember")}>Don't remember your password ? </p>
-          <Link to="/resetPassword" className={cx("textReset")}>
+          <Link to="/forgetPassword" className={cx("textReset")}>
             Reset it now
           </Link>
         </div>
