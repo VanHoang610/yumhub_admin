c84d8bc3f0536b9e9db5f9dccb9ebea3bdf4af4e
VanHoang610 <137982306+VanHoang610@users.noreply.github.com>
2024-05-23 19:46:48 +0700

    24/5

--- src/component/layouts/yumhub/AllMerchant/AllMerchant.js
+++ src/component/layouts/yumhub/AllMerchant/AllMerchant.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
+import Swal from "sweetalert2";
 
 import AxiosInstance from "../../../../utils/AxiosInstance";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,24 +13,174 @@ import {
 import classNames from "classnames/bind";
 import styles from "./AllMerchant.module.scss";
 import logo from "../../../../assets/images/logoYumhub.png";
+import image_merchant from "../../../../assets/images/logo_merchant.png";
+import ellipse from "../../../../assets/images/ellipse.png";
+import Button from "../../../buttons";
 
 const cx = classNames.bind(styles);
 
 function AllMerchant() {
   const [data, setData] = useState([{}]);
+  const [selectMerchantById, setSelectMerchantId] = useState({});
+  const [isEditModal, setIsEditModal] = useState(false);
 
+  const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [closeTime, setCloseTime] = useState("");
+  const [openTime, setOpenTime] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [type, setType] = useState("");
+  const [document, setDocument] = useState("");
+  const [typeId, setTypeId] = useState("");
+  const [types, setTypes] = useState([]);
+
+  // load data show lên page
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await AxiosInstance.get("merchants/getAllMerchant");
-        console.log(response.data.merchants);
-        setData(response.data.merchants);
+        const merchants = response.data.merchants;
+        setData(merchants);
+
+        const responseType = await AxiosInstance.get(
+          "merchants/getAllTypeOfMerchant"
+        );
+        setTypes(responseType.data.types);
       } catch (error) {
+        setTypes([]);
         console.log(error);
       }
     };
     fetchData();
   }, []);
+
+  //load data hiển thị lên modal
+  useEffect(() => {
+    if (selectMerchantById) {
+      setName(selectMerchantById.name || "");
+      setAddress(selectMerchantById.address || "");
+      setCloseTime(selectMerchantById.closeTime || "");
+      setOpenTime(selectMerchantById.openTime || "");
+      setPhoneNumber(
+        selectMerchantById.user ? selectMerchantById.user.phoneNumber : ""
+      );
+      setFullName(
+        selectMerchantById.user ? selectMerchantById.user.fullName : ""
+      );
+      setDocument(
+        selectMerchantById.document ? selectMerchantById.document.image : ""
+      );
+      setEmail(selectMerchantById.user ? selectMerchantById.user.email : "");
+      setType(selectMerchantById.type ? selectMerchantById.type.name : ""); // show type
+    }
+  }, [selectMerchantById]);
+
+  const handleEdit = async (id) => {
+    try {
+      const response = await AxiosInstance.get(`merchants/?id=${id}`);
+      const { detailMerchant } = response.data;
+      if (detailMerchant) {
+        setSelectMerchantId(detailMerchant);
+        setIsEditModal(true);
+        setShowModal(true);
+      } else {
+        console.log("Không tìm thấy thông tin ");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      customClass: {
+        popup: cx("swal2-popup"),
+        title: cx("swal2-title"),
+        confirmButton: cx("swal2-confirm"),
+        cancelButton: cx("swal2-cancel"),
+      },
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const response = await AxiosInstance.post(
+            `merchants/deleteMerchant/?id=${id}`
+          );
+          console.log(response);
+          setData(data.filter((merchant) => merchant._id !== id));
+          Swal.fire("Deleted!", "Your merchant has been deleted.", "success");
+        } catch (error) {
+          console.error("Failed to delete merchant:", error);
+          Swal.fire("Error!", "Failed to delete merchant.", "error");
+        }
+      }
+    });
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+    setIsEditModal(false);
+  };
+
+  const handleView = async (id) => {
+    try {
+      const response = await AxiosInstance.get(`merchants/?id=${id}`);
+      const { detailMerchant } = response.data;
+      if (detailMerchant) {
+        console.log(detailMerchant);
+        setSelectMerchantId(detailMerchant);
+        setShowModal(true);
+        setIsEditModal(false);
+      } else {
+        console.log("Không tìm thấy thông tin ");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const updateData = {
+        name,
+        address,
+        closeTime,
+        openTime,
+        phoneNumber,
+        fullName,
+        email,
+        type: typeId,
+      };
+
+      const response = await AxiosInstance.patch(
+        `merchants/updateMerchant?id=${selectMerchantById._id}`,
+        updateData
+      );
+
+      const updatedMerchant = response.data.merchantNew;
+      setData((prevData) =>
+        prevData.map((merchant) =>
+          merchant._id === updatedMerchant._id ? updatedMerchant : merchant
+        )
+      );
+      setShowModal(false);
+      setIsEditModal(false);
+      Swal.fire("Success", "Merchant updated successfully.", "success");
+    } catch (error) {
+      console.error("Failed to update merchant:", error);
+      Swal.fire("Error", "Failed to update merchant.", "error");
+    }
+  };
+
   return (
     <div className={cx("contaienr")}>
       <div className={cx("content")}>
@@ -41,27 +193,26 @@ function AllMerchant() {
           <input className={cx("input")} placeholder="Đồ ăn chay" />
         </div>
         <div className={cx("line-background")} />
-
         <div className={cx("box-container")}>
           <table className={cx("table")}>
             <thead>
               <tr>
                 <th>#</th>
-                <th className={cx("title-box")}>Name</th>
-                <th className={cx("title-box")}>Address</th>
-                <th className={cx("title-box")}>Time</th>
-                <th className={cx("title-box")}>Image</th>
-                <th className={cx("title-box")}>Actions</th>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Time</th>
+                <th>Image</th>
+                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
               {data.map((item, index) => (
                 <tr key={index} className={cx("table-row")}>
                   <td>{index + 1}</td>
-                  <td className={cx("title-box")}>{item.name}</td>
-                  <td className={cx("title-box")}>{item.address}</td>
-                  <td className={cx("title-box")}>{item.openTime}</td>
-                  <td className={cx("title-box")}>
+                  <td>{item.name}</td>
+                  <td>{item.address}</td>
+                  <td>{item.openTime}</td>
+                  <td>
                     <img
                       src={logo}
                       alt={`${item.name} logo`}
@@ -69,13 +220,22 @@ function AllMerchant() {
                     />
                   </td>
                   <td>
-                    <button className={cx("action-button")}>
+                    <button
+                      className={cx("action-button")}
+                      onClick={() => handleEdit(item._id)}
+                    >
                       <FontAwesomeIcon icon={faEdit} />
                     </button>
-                    <button className={cx("action-button")}>
+                    <button
+                      className={cx("action-button")}
+                      onClick={() => handleDelete(item._id)}
+                    >
                       <FontAwesomeIcon icon={faTrash} />
                     </button>
-                    <button className={cx("action-button")}>
+                    <button
+                      className={cx("action-button")}
+                      onClick={() => handleView(item._id)}
+                    >
                       <FontAwesomeIcon icon={faEye} />
                     </button>
                   </td>
@@ -84,6 +244,165 @@ function AllMerchant() {
             </tbody>
           </table>
         </div>
+        <Modal
+          isOpen={showModal}
+          onRequestClose={handleModalClose}
+          contentLabel="Update Merchant"
+          className={cx("modal")}
+        >
+          {selectMerchantById && (
+            <div className={cx("modal-container")}>
+              <div className={cx("logo-merchant")}>
+                <img src={ellipse} alt="Ellipse" className={cx("ellipse")} />
+                <img
+                  src={image_merchant}
+                  alt="Merchant"
+                  className={cx("img-merchant")}
+                />
+              </div>
+              <div className={cx("content")}>
+                {!isEditModal ? <Button reviewed>Reviewed</Button> : ""}
+                <div className={cx("container-content")}>
+                  <p className={cx("name-merchant")}>
+                    {isEditModal ? (
+                      <input
+                        className={cx("name-merchant")}
+                        name="name"
+                        defaultValue={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    ) : (
+                      name
+                    )}
+                  </p>
+                  <div className={cx("line")}></div>
+                  <p className={cx("type-merchant")}>
+                    {isEditModal ? (
+                      <select
+                        className={cx("type-merchant")}
+                        value={typeId}
+                        onChange={(e) => setTypeId(e.target.value)}
+                      >
+                        {types.map((type) => (
+                          <option key={type._id} value={type._id}>
+                            {type.name}
+                          </option>
+                        ))}
+                      </select>
+                    ) : (
+                      type || "N/A"
+                    )}
+                  </p>
+                </div>
+                <div className={cx("wrapper-content")}>
+                  <p className={cx("title-merchant")}>Address:</p>
+                  <p className={cx("content-merchant")}>
+                    {isEditModal ? (
+                      <input
+                        className={cx("content-merchant")}
+                        name="address"
+                        defaultValue={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                      />
+                    ) : (
+                      address
+                    )}
+                  </p>
+                </div>
+                <div className={cx("wrapper-content")}>
+                  <p className={cx("title-merchant")}>Email:</p>
+                  <p className={cx("content-merchant")}>
+                    {isEditModal ? (
+                      <input
+                        className={cx("content-merchant")}
+                        name="email"
+                        defaultValue={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    ) : (
+                      email
+                    )}
+                  </p>
+                </div>
+                <div className={cx("wrapper-content")}>
+                  <p className={cx("title-merchant")}>Store Owner:</p>
+                  <p className={cx("content-merchant")}>
+                    {isEditModal ? (
+                      <input
+                        className={cx("content-merchant")}
+                        name="fullName"
+                        defaultValue={fullName}
+                        onChange={(e) => setFullName(e.target.value)}
+                      />
+                    ) : (
+                      fullName
+                    )}
+                  </p>
+                </div>
+                <div className={cx("wrapper-content")}>
+                  <p className={cx("title-merchant")}>PhoneNumber:</p>
+                  <p className={cx("content-merchant")}>
+                    {isEditModal ? (
+                      <input
+                        className={cx("content-merchant")}
+                        name="phoneNumber"
+                        defaultValue={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                      />
+                    ) : (
+                      phoneNumber
+                    )}
+                  </p>
+                </div>
+                <div className={cx("wrapper-content")}>
+                  <p className={cx("title-merchant")}>Open Time:</p>
+                  <p className={cx("content-merchant")}>
+                    {isEditModal ? (
+                      <input
+                        className={cx("content-merchant")}
+                        name="openTime"
+                        defaultValue={openTime}
+                        onChange={(e) => setOpenTime(e.target.value)}
+                      />
+                    ) : (
+                      openTime
+                    )}
+                  </p>
+                </div>
+                <div className={cx("wrapper-content")}>
+                  <p className={cx("title-merchant")}>Close Time:</p>
+                  <p className={cx("content-merchant")}>
+                    {isEditModal ? (
+                      <input
+                        className={cx("content-merchant")}
+                        name="closeTime"
+                        defaultValue={closeTime}
+                        onChange={(e) => setCloseTime(e.target.value)}
+                      />
+                    ) : (
+                      closeTime
+                    )}
+                  </p>
+                </div>
+                <div className={cx("wrapper-image-content")}>
+                  <p className={cx("title-merchant")}>Document:</p>
+                  <p className={cx("content-merchant")}>
+                    {isEditModal ? "" : <img src={document} alt="Document" className={cx("image-document")}/>}
+                  </p>
+                </div>
+                <div className={cx("btn-delete")}>
+                  {isEditModal ? (
+                    <Button approve_btn onClick={handleUpdate}>
+                      Update
+                    </Button>
+                  ) : (
+                    <Button delete_btn>Deleted</Button>
+                  )}
+                </div>
+              </div>
+            </div>
+          )}
+        </Modal>
       </div>
     </div>
   );
--- src/component/layouts/yumhub/NewMerchant/NewMerchant.js
+++ src/component/layouts/yumhub/NewMerchant/NewMerchant.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "../../../buttons/index";
@@ -11,11 +12,64 @@ import AxiosInstance from "../../../../utils/AxiosInstance";
 import classNames from "classnames/bind";
 import styles from "./NewMerchant.module.scss";
 import logo from "../../../../assets/images/logoYumhub.png";
+import image_merchant from "../../../../assets/images/logo_merchant.png";
+import ellipse from "../../../../assets/images/ellipse.png";
 
 const cx = classNames.bind(styles);
 function NewMerchant() {
   const [data, setData] = useState([]);
+  const [selectMerchantById, setSelectMerchantId] = useState({});
+  const [isEditModal, setIsEditModal] = useState(false);
 
+  const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [closeTime, setCloseTime] = useState("");
+  const [openTime, setOpenTime] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [type, setType] = useState("");
+  const [document, setDocument] = useState("");
+
+  const selectDetail = async (id) => {
+    try {
+      const response = await AxiosInstance.get(`merchants/?id=${id}`);
+      const { detailMerchant } = response.data;
+      if (detailMerchant) {
+        setSelectMerchantId(detailMerchant);
+        setShowModal(true);
+        setIsEditModal(false);
+      } else {
+        console.log("Không tìm thấy thông tin ");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //load data hiển thị lên modal
+  useEffect(() => {
+    if (selectMerchantById) {
+      setName(selectMerchantById.name || "");
+      setAddress(selectMerchantById.address || "");
+      setCloseTime(selectMerchantById.closeTime || "");
+      setOpenTime(selectMerchantById.openTime || "");
+      setPhoneNumber(
+        selectMerchantById.user ? selectMerchantById.user.phoneNumber : ""
+      );
+      setFullName(
+        selectMerchantById.user ? selectMerchantById.user.fullName : ""
+      );
+      setDocument(
+        selectMerchantById.document ? selectMerchantById.document.image : ""
+      );
+      setEmail(selectMerchantById.user ? selectMerchantById.user.email : "");
+      setType(selectMerchantById.type ? selectMerchantById.type.name : ""); // show type
+    }
+  }, [selectMerchantById]);
+
+  //list mercahnt
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -30,6 +84,7 @@ function NewMerchant() {
     };
     fetchData();
   }, []);
+
   return (
     <div className={cx("contaienr")}>
       <div className={cx("content")}>
@@ -50,7 +105,7 @@ function NewMerchant() {
                 <div className={cx("line")} />
                 <div className={cx("textTitle")}>
                   <p className={cx("nameMerchant")}>{item.name}</p>
-                  <p className={cx("type")}>Đồ ăn chay</p>
+                  <p className={cx("type")}>{item.type.name}</p>
                 </div>
               </div>
               <div className={cx("line-bottom")} />
@@ -65,12 +120,79 @@ function NewMerchant() {
                 </div>
               </div>
               <div className={cx("btn-detail")}>
-                <Button detail>Detail</Button>
+                <Button
+                  detail
+                  onClick={() => {
+                    selectDetail(item._id);
+                  }}
+                >
+                  Detail
+                </Button>
               </div>
             </div>
           ))}
         </div>
       </div>
+      <Modal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(false)}
+        contentLabel="Update Merchant"
+        className={cx("modal")}
+      >
+        {selectMerchantById && (
+          <div className={cx("modal-container")}>
+            <div className={cx("logo-merchant")}>
+              <img src={ellipse} alt="Ellipse" className={cx("ellipse")} />
+              <img
+                src={image_merchant}
+                alt="Merchant"
+                className={cx("img-merchant")}
+              />
+            </div>
+            <div className={cx("content-modal")}>
+              <Button awaiting>Awaiting Approve</Button>
+              <div className={cx("container-content")}>
+                <p className={cx("name-merchant")}>{name}</p>
+                <div className={cx("line")}></div>
+                <p className={cx("type-merchant")}>{type}</p>
+              </div>
+              <div className={cx("wrapper-content")}>
+                <p className={cx("title-merchant")}>Address:</p>
+                <p className={cx("content-merchant")}>{address}</p>
+              </div>
+              <div className={cx("wrapper-content")}>
+                <p className={cx("title-merchant")}>Email:</p>
+                <p className={cx("content-merchant")}>{email}</p>
+              </div>
+              <div className={cx("wrapper-content")}>
+                <p className={cx("title-merchant")}>Store Owner:</p>
+                <p className={cx("content-merchant")}>{fullName}</p>
+              </div>
+              <div className={cx("wrapper-content")}>
+                <p className={cx("title-merchant")}>PhoneNumber:</p>
+                <p className={cx("content-merchant")}>{phoneNumber}</p>
+              </div>
+              <div className={cx("wrapper-content")}>
+                <p className={cx("title-merchant")}>Open Time:</p>
+                <p className={cx("content-merchant")}>{openTime}</p>
+              </div>
+              <div className={cx("wrapper-content")}>
+                <p className={cx("title-merchant")}>Close Time:</p>
+                <p className={cx("content-merchant")}>{closeTime}</p>
+              </div>
+              <div className={cx("wrapper-image-content")}>
+                  <p className={cx("title-merchant")}>Document:</p>
+                  <p className={cx("content-merchant")}>
+                    <img src={document} alt="Document" className={cx("image-document")}/>
+                  </p>
+                </div>
+              <div className={cx("btn-delete")}>
+                <Button approve_btn>Approval</Button>
+              </div>
+            </div>
+          </div>
+        )}
+      </Modal>
     </div>
   );
 }
